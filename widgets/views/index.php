ff9591f13047cbf676a6e239c685f8edfcf37280
Igor Chepurnoy <igorzfort@gmail.com>
2015-10-09 14:14:09 +0300

    Update index.php

--- widgets/views/index.php
+++ widgets/views/index.php
@@ -1,14 +1,22 @@
 <?php
 /* @var $this \yii\web\View */
+use yii\widgets\Pjax;
+
 /* @var $comments array */
 /* @var $commentModel \yii2mod\comments\models\CommentModel */
 /* @var $maxLevel null|integer comments max level */
 /* @var $encryptedEntity string */
+/* @var $pjaxContainerId string */
 ?>
+<?php Pjax::begin([
+    'enablePushState' => false,
+    'timeout' => 10000,
+    'id' => $pjaxContainerId,
+]); ?>
 <div class="comments row">
     <div class="col-md-11 col-sm-11">
         <div class="title-block clearfix">
-            <h3 class="h3-body-title"><?php echo 'Comments'; ?></h3>
+            <h3 class="h3-body-title"><?php echo Yii::t('app', 'Comments'); ?></h3>
 
             <div class="title-seperator"></div>
         </div>
@@ -20,5 +28,5 @@
         <?php endif; ?>
     </div>
 </div>
-
+<?php Pjax::end(); ?>
 

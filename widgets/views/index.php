4fcdbc20f09d7a2c82c23f2bbfb7c53e9bc88d52
Igor Chepurnoy <igorzfort@gmail.com>
2015-12-14 18:15:29 +0200

    Update index.php

--- widgets/views/index.php
+++ widgets/views/index.php
@@ -1,30 +1,35 @@
 <?php
-/* @var $this \yii\web\View */
+
 use yii\widgets\Pjax;
 
+/* @var $this \yii\web\View */
 /* @var $comments array */
 /* @var $commentModel \yii2mod\comments\models\CommentModel */
 /* @var $maxLevel null|integer comments max level */
 /* @var $encryptedEntity string */
 /* @var $pjaxContainerId string */
+/* @var $formId string comment form id */
 ?>
 <?php Pjax::begin([
     'enablePushState' => false,
     'timeout' => 10000,
-    'id' => $pjaxContainerId,
+    'id' => $pjaxContainerId
 ]); ?>
 <div class="comments row">
-    <div class="col-md-11 col-sm-11">
+    <div class="col-md-12 col-sm-12">
         <div class="title-block clearfix">
             <h3 class="h3-body-title"><?php echo Yii::t('app', 'Comments'); ?></h3>
-
-            <div class="title-seperator"></div>
+            <div class="title-separator"></div>
         </div>
         <ol class="comments-list">
             <?php echo $this->render('_list', ['comments' => $comments, 'maxLevel' => $maxLevel]) ?>
         </ol>
         <?php if (!\Yii::$app->user->isGuest) : ?>
-            <?php echo $this->render('_form', ['commentModel' => $commentModel, 'encryptedEntity' => $encryptedEntity]); ?>
+            <?php echo $this->render('_form', [
+                'commentModel' => $commentModel,
+                'encryptedEntity' => $encryptedEntity,
+                'formId' => $formId
+            ]); ?>
         <?php endif; ?>
     </div>
 </div>

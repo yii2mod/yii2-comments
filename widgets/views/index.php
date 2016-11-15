1e0590953547154d4c80de834993b0191b4097e5
igor-chepurnoi <igorzfort@gmail.com>
2016-11-15 18:57:19 +0200

    fix PSR-2 inspections

--- widgets/views/index.php
+++ widgets/views/index.php
@@ -16,14 +16,14 @@ use yii\widgets\Pjax;
     <div class="col-md-12 col-sm-12">
         <div class="title-block clearfix">
             <h3 class="h3-body-title">
-                <?php echo Yii::t('yii2mod.comments', "Comments ({0})", $commentModel->getCommentsCount($showDeletedComments ? false : true)); ?>
+                <?php echo Yii::t('yii2mod.comments', 'Comments ({0})', $commentModel->getCommentsCount($showDeletedComments ? false : true)); ?>
             </h3>
             <div class="title-separator"></div>
         </div>
         <ol class="comments-list">
-            <?php echo $this->render('_list', ['comments' => $comments, 'maxLevel' => $maxLevel]) ?>
+            <?php echo $this->render('_list', ['comments' => $comments, 'maxLevel' => $maxLevel]); ?>
         </ol>
-        <?php if (!Yii::$app->user->isGuest): ?>
+        <?php if (!Yii::$app->user->isGuest) : ?>
             <?php echo $this->render('_form', [
                 'commentModel' => $commentModel,
                 'encryptedEntity' => $encryptedEntity,

20e28db275c79894cdbdfee054b5a8bab033ed34
Igor Chepurnoy <igorzfort@gmail.com>
2017-03-13 13:58:02 +0200

    Update m161109_092304_rename_comment_table.php

--- migrations/m161109_092304_rename_comment_table.php
+++ migrations/m161109_092304_rename_comment_table.php
@@ -6,14 +6,14 @@ class m161109_092304_rename_comment_table extends Migration
 {
     public function up()
     {
-        if (Yii::$app->db->schema->getTableSchema('comment') === null) {
+        if (Yii::$app->db->schema->getTableSchema('{{%comment}}') === null) {
             $this->renameTable('{{%Comment}}', '{{%comment}}');
         }
     }
 
     public function down()
     {
-        if (Yii::$app->db->schema->getTableSchema('Comment') === null) {
+        if (Yii::$app->db->schema->getTableSchema('{{%Comment}}') === null) {
             $this->renameTable('{{%comment}}', '{{%Comment}}');
         }
     }
